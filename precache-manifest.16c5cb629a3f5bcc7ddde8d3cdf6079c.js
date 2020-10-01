5826498a7c59d01ef693d869c3b3716b02dc5a90
Ismail Khan <ismailislamkhan@gmail.com>
2020-10-01 01:15:31 -0400

    Updates

---
+++ precache-manifest.16c5cb629a3f5bcc7ddde8d3cdf6079c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d68806f2c19da922593a183935bab0d0",
+    "url": "/ismailkhanio/ismail-khan-project-five/index.html"
+  },
+  {
+    "revision": "073eca77db95fb5ead9c",
+    "url": "/ismailkhanio/ismail-khan-project-five/static/css/main.0ba56110.chunk.css"
+  },
+  {
+    "revision": "2a163e9d217832b4bcd6",
+    "url": "/ismailkhanio/ismail-khan-project-five/static/js/2.4c179b12.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/ismailkhanio/ismail-khan-project-five/static/js/2.4c179b12.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "073eca77db95fb5ead9c",
+    "url": "/ismailkhanio/ismail-khan-project-five/static/js/main.ac6021eb.chunk.js"
+  },
+  {
+    "revision": "5d5819b10e3bfbd293ca",
+    "url": "/ismailkhanio/ismail-khan-project-five/static/js/runtime-main.12f74c26.js"
+  },
+  {
+    "revision": "ceeb9132936ed8538ae78872b4266ae6",
+    "url": "/ismailkhanio/ismail-khan-project-five/static/media/logo.ceeb9132.svg"
+  }
+]);
